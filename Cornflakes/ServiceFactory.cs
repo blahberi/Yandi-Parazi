1b9e240eea075765c5bb08ec95787e87329109cd
blahberi <eitan.hanson@gmail.com>
2024-10-15 20:49:38 +0300

    optimized factory generation

--- Cornflakes/ServiceFactory.cs
+++ Cornflakes/ServiceFactory.cs
@@ -13,18 +13,26 @@ namespace Cornflakes.LifetimeStrategies
         {
             return GenerateFactory(typeof(TImplementation));
         }
+        private static object CreateInstance(Type implementationType, IServiceProvider serviceProvider)
+        {
+            object[] constructorParameters = implementationType.GetConstructors().First()
+                .GetParameters()
+                .Select(p => serviceProvider.GetService(p.ParameterType))
+                .ToArray();
+            return Activator.CreateInstance(implementationType, constructorParameters);
+        }
 
         private static ServiceFactory GenerateFactory(Type implementationType)
         {
             ParameterExpression serviceProviderParameter = Expression.Parameter(typeof(IServiceProvider));
+            MethodInfo GetService = typeof(IServiceProvider).GetMethod(nameof(IServiceProvider.GetService));
 
             ConstructorInfo constructor = implementationType.GetConstructors().First();
             IEnumerable<Expression> arguments = constructor.GetParameters()
                 .Select(p => Expression.Convert(
                     Expression.Call(
                         serviceProviderParameter,
-                        typeof(IServiceProvider)
-                        .GetMethod(nameof(IServiceProvider.GetService)),
+                        GetService,
                         Expression.Constant(p.ParameterType)),
                     p.ParameterType));
 
